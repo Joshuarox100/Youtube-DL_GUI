93f90316ce6a494aae2afcd5910c031e381ab460
Joshuarox100 <joshuabender10@gmail.com>
2019-08-17 21:16:39 -0500

    Improved Readability

--- Downloader.java
+++ Downloader.java
@@ -24,64 +24,80 @@ public class Downloader extends Thread
     public void downloadVideo(String format, String URL, int start, int end, JTextPane console)
     {   
         // Beginning of command
-        String command = "data\\bin\\ytdl\\youtube-dl.exe ";
-
-        // Audio Formats
-        if (format.equals("mp3"))
-            command = command + "-x --audio-format mp3 ";
-        else if (format.equals("aac"))
-            command = command + "-x --audio-format aac ";
-        else if (format.equals("flac"))
-            command = command + "-x --audio-format flac ";
-        else if (format.equals("m4a"))
-            command = command + "-x --audio-format m4a "; 
-        else if (format.equals("opus"))
-            command = command + "-x --audio-format opus ";
-        else if (format.equals("vorbis"))
-            command = command + "-x --audio-format vorbis ";
-        else if (format.equals("wav"))
-            command = command + "-x --audio-format wav ";
-
-        // Video Formats
-        else if (format.equals("mp4"))
-            command = command + "--recode-video mp4 ";
-        else if (format.equals("flv"))
-            command = command + "--recode-video flv ";
-        else if (format.equals("ogg"))
-            command = command + "--recode-video ogg ";
-        else if (format.equals("webm"))
-            command = command + "--recode-video webm ";
-        else if (format.equals("mkv"))
-            command = command + "--recode-video mkv ";
-        else if (format.equals("avi"))
-            command = command + "--recode-video avi ";   
+        StringBuilder builder = new StringBuilder();
+        builder.append("data\\bin\\ytdl\\youtube-dl.exe ");
+        // Learning Switch
+        switch(format) {
+            // Audio Formats
+            case "mp3":
+                builder.append("-x --audio-format mp3 ");
+                break;
+            case "aac":
+                builder.append("-x --audio-format aac ");
+                break;
+            case "flac":
+                builder.append("-x --audio-format flac ");
+                break;
+            case "m4a":
+                builder.append("-x --audio-format m4a ");
+                break;
+            case "opus":
+                builder.append("-x --audio-format opus ");
+                break;
+            case "vorbis":
+                builder.append("-x --audio-format vorbis ");
+                break;
+            case "wav":
+                builder.append("-x --audio-format wav ");
+                break;
+                
+            // Video Formats    
+            case "mp4":
+                builder.append("--recode-video mp4 ");
+                break;
+            case "flv":
+                builder.append("--recode-video flv ");
+                break;
+            case "ogg":
+                builder.append("--recode-video ogg ");
+                break;
+            case "webm":
+                builder.append("--recode-video webm ");
+                break;
+            case "mkv":
+                builder.append("--recode-video mkv ");
+                break;
+            case "avi":
+                builder.append("--recode-video avi ");
+                break;
+        }
 
         try
         {
             String[] config = Settings.loadSettings();
             if (!config[0].equals(""))
-                command = command + "-o \"" + config[0] + "%(title)s.%(ext)s\" ";
+                builder.append("-o \"" + config[0] + "%(title)s.%(ext)s\" ");
             if (Boolean.parseBoolean(config[1]))
-                command = command + "--no-playlist ";
+                builder.append("--no-playlist ");
             if (Boolean.parseBoolean(config[2]))
-                command = command + "-i ";
+                builder.append("-i ");
             if (!Boolean.parseBoolean(config[3]))
-                command = command + "-w ";
+                builder.append("-w ");
             if (start > 0)
-                command = command + "--playlist-start " + start + " ";
+                builder.append("--playlist-start " + start + " ");
             if (end > 0)
-                command = command + "--playlist-end " + end + " ";
+                builder.append("--playlist-end " + end + " ");
         }
         catch (Exception e)
         {
             console.setText(console.getText() + "Failed to Load Configuration File, Proceeding with Default Settings... \n");
         }
 
-        command = command + "--ignore-config --ffmpeg-location \"data\\bin\\ffmpeg\\bin\\ffmpeg.exe\" ";
+        builder.append("--ignore-config --ffmpeg-location \"data\\bin\\ffmpeg\\bin\\ffmpeg.exe\" ");
 
         // Making sure there is a URL entered
         if (URL.length() != 0 && URL.indexOf(" ") == -1)
-            command = command + "\"" + URL + "\"";
+            builder.append("\"" + URL + "\"");
         else
         {
             console.setText(console.getText() + "Invalid URL. Skiping... \n");
@@ -96,7 +112,7 @@ public class Downloader extends Thread
         { 
             // Launch command prompt and run command
             Main.menu.liveOut.setText("Downloading...");
-            ProcessBuilder cmd = new ProcessBuilder(new String[] {"cmd", "/C", command + " && exit"});
+            ProcessBuilder cmd = new ProcessBuilder(new String[] {"cmd", "/C", builder.toString() + " && exit"});
             cmd.redirectErrorStream(true);
             proc = cmd.start();
             // Read the output and error log from command prompt
